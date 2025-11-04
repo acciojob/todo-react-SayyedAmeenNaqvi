5e9948cbb704f1518efb272bc895da1c5da0bfa5
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-11-04 18:05:25 +0530

    Implement todo management in App component
    Added functionality to manage todos with add and delete options.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import ToDoList from "./ToDoList";
+import Input from "./Input";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (text) => {
+    setTodos([...todos, text]);
+  }
+
+  function handleDelete(index){
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  }
+
   return (
     <div>
-      <ToDoList />
+        <h1>To-Do List</h1>
+        <Input onAdd={handleAddTodo} />
+        <ul>
+            {todos.map((todo, index) => (
+                <li key={index}>
+                    {todo}
+                    <button onClick={() => handleDelete(index)}>Delete</button>
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
 
 export default App
+
